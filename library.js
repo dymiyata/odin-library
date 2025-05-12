e302411ec250fa1a82de9db97ef74a3017700994
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-05-12 11:40:01 -0700

    Add functionality to new book dialog

--- library.js
+++ library.js
@@ -1,27 +1,17 @@
 const myLibrary = [];
 const tbodyElement = document.querySelector("tbody");
-const newBookButton = document.querySelector("#new-book-button");
-const newBookDialog = document.querySelector("#new-book-dialog");
-const closeDialogButton = document.querySelector("#close-dialog");
 
-newBookButton.addEventListener("click", () => {
-    newBookDialog.showModal();
-});
 
-closeDialogButton.addEventListener("click", () => {
-    newBookDialog.close();
-});
 
-function Book(title, author, pages, read) {
+function Book(title, author, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    newbook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, read) {
+    newbook = new Book(title, author, read);
     myLibrary.push(newbook);
 }
 
@@ -38,12 +28,52 @@ function generateTableRow(book) {
 }
 
 function generateTable() {
+    while (tbodyElement.firstChild) {
+        tbodyElement.removeChild(tbodyElement.lastChild);
+    }
     myLibrary.forEach(element => {
         generateTableRow(element);
     });
 }
 
-addBookToLibrary("The Hobbit", "Dane Miyata", 199, true);
-addBookToLibrary("Project Hail Mary", "some guy", 245, true);
-addBookToLibrary("Woah this is a book", "Annie Wang", 800, false);
-addBookToLibrary("A Dog's Tale", "Ollie the beagle", 610, true);
+addBookToLibrary("The Hobbit", "Dane Miyata", true);
+addBookToLibrary("Project Hail Mary", "some guy", true);
+addBookToLibrary("Woah this is a book", "Annie Wang", false);
+addBookToLibrary("A Dog's Tale", "Ollie the beagle", true);
+generateTable();
+
+
+
+
+const newBookButton = document.querySelector("#new-book-button");
+const newBookDialog = document.querySelector("#new-book-dialog");
+const closeDialogButton = document.querySelector("#close-dialog");
+const newBookTitleInput = document.querySelector("#ftitle");
+const newBookAuthorInput = document.querySelector("#fauthor");
+const readCheckbox = document.querySelector("#fread");
+const submitNewBookButton = document.querySelector("#fsubmit");
+
+
+newBookButton.addEventListener("click", () => {
+    newBookDialog.showModal();
+});
+
+closeDialogButton.addEventListener("click", () => {
+    newBookDialog.close();
+});
+
+submitNewBookButton.addEventListener("click", (event) => submitNewBook(event));
+
+function submitNewBook(event) {
+    addBookToLibrary(newBookTitleInput.value, newBookAuthorInput.value, readCheckbox.checked);
+    generateTable();
+    clearDialog();
+    event.preventDefault();
+    newBookDialog.close();
+};
+
+function clearDialog() {
+    newBookTitleInput.value = "";
+    newBookAuthorInput.value = "";
+    readCheckbox.checked = false;
+}
