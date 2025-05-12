4b6a19c5c9a5f0d4a6c2265b8d6485fdc04103df
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-05-12 10:53:32 -0700

    Create Functionality to display table
    Given an array of books, create function to display title and author of the books in a table.

---
+++ library.js
@@ -0,0 +1,38 @@
+const myLibrary = [];
+const tbodyElement = document.querySelector("tbody");
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    newbook = new Book(title, author, pages, read);
+    myLibrary.push(newbook);
+}
+
+function generateTableRow(book) {
+    const trElement = document.createElement("tr");
+    const titleCellElement = document.createElement("td");
+    const authorCellElement = document.createElement("td");
+    trElement.id = book.id;
+    trElement.appendChild(titleCellElement);
+    trElement.appendChild(authorCellElement);
+    titleCellElement.innerText = book.title;
+    authorCellElement.innerText = book.author;
+    tbodyElement.append(trElement);
+}
+
+function generateTable() {
+    myLibrary.forEach(element => {
+        generateTableRow(element);
+    });
+}
+
+addBookToLibrary("The Hobbit", "Dane Miyata", 199, true);
+addBookToLibrary("Project Hail Mary", "some guy", 245, true);
+addBookToLibrary("Woah this is a book", "Annie Wang", 800, false);
+addBookToLibrary("A Dog's Tale", "Ollie the beagle", 610, true);
