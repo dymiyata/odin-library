a756fd919c33cc56eda73a1a5fb05115e32cd570
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-05-13 10:49:38 -0700

    Implement remove book buttons

--- library.js
+++ library.js
@@ -11,20 +11,44 @@ function Book(title, author, read) {
 }
 
 function addBookToLibrary(title, author, read) {
-    newbook = new Book(title, author, read);
-    myLibrary.push(newbook);
+    newBook = new Book(title, author, read);
+    myLibrary.push(newBook);
+    generateTableRow(newBook);
 }
 
 function generateTableRow(book) {
     const trElement = document.createElement("tr");
     const titleCellElement = document.createElement("td");
     const authorCellElement = document.createElement("td");
+    const readCell = document.createElement("td");
     trElement.id = book.id;
     trElement.appendChild(titleCellElement);
     trElement.appendChild(authorCellElement);
+    trElement.appendChild(readCell);
     titleCellElement.innerText = book.title;
     authorCellElement.innerText = book.author;
+    readCell.innerText = book.read ? "read" : "not read";
     tbodyElement.append(trElement);
+
+
+    const buttonCell = document.createElement("td");
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.setAttribute("data-book-id", book.id);
+    trElement.appendChild(buttonCell);
+    buttonCell.appendChild(removeButton);
+    removeButton.addEventListener("click", () => deleteCurrentBook(book.id));
+}
+
+function deleteCurrentBook(bookId) {
+    const tableRow = document.getElementById(bookId);
+    tableRow.remove();
+    for (let i = myLibrary.length - 1; i >= 0; i--) {
+        if (myLibrary[i].id === bookId) {
+            myLibrary.splice(i, 1);
+            break;
+        }
+    }
 }
 
 function generateTable() {
@@ -66,7 +90,7 @@ submitNewBookButton.addEventListener("click", (event) => submitNewBook(event));
 
 function submitNewBook(event) {
     addBookToLibrary(newBookTitleInput.value, newBookAuthorInput.value, readCheckbox.checked);
-    generateTable();
+    // generateTable();
     clearDialog();
     event.preventDefault();
     newBookDialog.close();
